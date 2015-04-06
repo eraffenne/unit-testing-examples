82903644602affc810ceb7725fb186b556c3e83d
Emmanuelle Raffenne <eraffenne@keedio.com>
2015-04-06 15:57:01 +0200

    Add examples of assumptions and assertions

---
+++ src/test/java/com/example/unittesting/AssertionTest.java
@@ -0,0 +1,53 @@
+package com.example.unittesting;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringStartsWith;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertionTest {
+
+    @Test
+    public void testEquals() {
+        String expected = "Hola";
+
+        String myTestString = "hola".replaceFirst("h", "H");
+        Assert.assertEquals("These have different values", expected, myTestString);
+    }
+
+    @Test
+    public void testArrayEquals() {
+        String[] expected = new String[]{"uno", "dos", "tres"};
+
+        String[] myTestArray = new String[]{"uno", "dos", "tres"};
+        Assert.assertArrayEquals("Arrays are different. Check the order of elements.", expected, myTestArray);
+    }
+
+    @Test
+    public void testTrue() {
+        Assert.assertTrue("String length is not the correct", "hola".length() == 4);
+    }
+
+    @Test
+    public void testNull() {
+        String myString = null;
+        Assert.assertNull("String already initialized", myString);
+    }
+
+    @Test
+    public void testNotSame() {
+        String expected = "Hola";
+
+        String myTestString = "hola".replaceFirst("h", "H");
+        Assert.assertNotSame("These are the same object.", expected, myTestString);
+
+    }
+
+    @Test
+    public void testThat() {
+        Matcher myMatcher = new StringStartsWith("H");
+
+        String myTestString = "Hola";
+        Assert.assertThat("First letter is not right.", myTestString, myMatcher);
+    }
+}
---
+++ src/test/java/com/example/unittesting/AssumptionTest.java
@@ -0,0 +1,25 @@
+package com.example.unittesting;
+
+import org.junit.Assert;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assume;
+import org.junit.Test;
+
+import java.io.File;
+
+public class AssumptionTest {
+
+    @Test
+    public void testAssumeThatIgnored() {
+        Assume.assumeThat("Test ignored.", String.valueOf(File.separatorChar), CoreMatchers.is("\\"));
+
+        Assert.assertEquals("Wrong path.", "/tmp/filename.ext", new File("/tmp/filename.ext").getAbsolutePath());
+    }
+
+    @Test
+    public void testAssumeThat() {
+        Assume.assumeThat("Test ignored.", String.valueOf(File.separatorChar), CoreMatchers.is("/"));
+
+        Assert.assertEquals("Wrong path.", "/tmp/filename.ext", new File("/tmp/filename.ext").getAbsolutePath());
+    }
+}
